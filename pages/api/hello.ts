b39cc67df962571dccb2c7767d70e17e575a1dc6
mizuide <mizuide.shimpei@gmail.com>
2022-05-14 23:26:53 +0900

    added prisma and updated api/hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+const prisma = new PrismaClient()
+BigInt.prototype.toJSON = function () {
+  const int = Number.parseInt(this.toString());
+  return int ?? this.toString();
+};
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const user = await prisma.users.findFirst({
+    where: {
+      id: 1
+    }
+  })
+  res.status(200).json(user)
 }
