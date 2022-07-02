3533ca10b9d281baa87045c7cfc4db54466126ae
mizuide <mizuide.shimpei@gmail.com>
2022-07-02 22:41:57 +0900

    fixed api url

--- components/hooks/useUserSWR.ts
+++ components/hooks/useUserSWR.ts
@@ -7,8 +7,9 @@ export const useAllUsersSWR = () => {
     const resonse = await fetch(url);
     return resonse.json();
   };
+  
   const { data, error } = useSWR<users>(
-    (typeof window !== 'undefined') ? "http://127.0.0.1:3001/api/hello" : "http://127.0.0.1:3000/api/hello",
+    ((typeof window !== 'undefined') ? location.origin : process.env.API_HOST_AT_SERVER) + "/api/hello",
     fetcher,
     { suspense: true }
   );
