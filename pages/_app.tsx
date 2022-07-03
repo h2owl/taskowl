2c35c034c0920c5c8f3eb42e0f4d6cbd5366e46a
mizuide <mizuide.shimpei@gmail.com>
2022-07-03 15:32:36 +0900

    added temporary loading display
    https://deecode.net/?p=1891

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,30 @@ import { SessionProvider } from "next-auth/react"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react';
+import { useRouter } from "next/router"
 
 function MyApp({ Component, pageProps:{ session, ...pageProps } }: AppProps) {
+  const router = useRouter()
+  const [pageLoading, setPageLoading] = useState(false)
+
+  useEffect(() => {
+    const handleStart = (url:string) => url !== router.asPath && setPageLoading(true)
+    const handleComplete = () => setPageLoading(false)
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
+  const loadingComponent = (<h2>Loading...</h2>)
   return <>
+      {pageLoading && loadingComponent}
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
